2dda0409e8d60c867ca82618ddaacecf4aa3f181
FallenMax <FallenMax@gmail.com>
2025-06-08 23:11:32 +0800

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,36 +2,38 @@
 
 set -e
 
+source .env.sh || echo 'no .env.sh file found'
+
 target=$1
-host=root@129.226.151.217
-port=22
+host=$DEPLOY_HOST
+port=$DEPLOY_PORT
 skip_build=${SKIP_BUILD}
 
 if [ "$target" == 'dev' ]; then
-	app_dir=/root/apps/notepad_dev
-	start_cmd="pm2 startOrRestart pm2_dev.json"
+  app_dir=/root/apps/notepad_dev
+  start_cmd="pm2 startOrRestart pm2_dev.json"
 elif [ "$target" = 'prod' ]; then
-	app_dir=/root/apps/notepad
-	start_cmd="pm2 startOrRestart pm2_prod.json"
+  app_dir=/root/apps/notepad
+  start_cmd="pm2 startOrRestart pm2_prod.json"
 else
-	echo 'unknown target'
-	exit 1
+  echo 'unknown target'
+  exit 1
 fi
 
 echo "deploying: $target"
 
 if [[ $skip_build == '1' ]]; then
-	echo 'skipping build'
+  echo 'skipping build'
 else
-	pnpm install
-	pnpm run type
-	pnpm run test --run
-	pnpm run build
+  pnpm install
+  pnpm run type
+  pnpm run test --run
+  pnpm run build
 fi
 
-rsync --exclude-from=.rsyncignore -avz ./ $host:$app_dir/
+rsync --exclude-from=.rsyncignore -avz ./ "$host":$app_dir/
 
-ssh $host -p$port <<EOF
+ssh "$host" -p"$port" <<EOF
   set -e
 
   cd $app_dir
